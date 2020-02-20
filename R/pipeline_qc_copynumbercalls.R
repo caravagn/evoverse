fd502e483c420e0068e76acbddc223c21037f02d
Giulio Caravagna <gcaravagn@gmail.com>
2020-02-20 11:42:19 +0000

    Pipeline CNAqc with reports

--- R/pipeline_qc_copynumbercalls.R
+++ R/pipeline_qc_copynumbercalls.R
@@ -1,16 +1,9 @@
-mutations = CNAqc::example_dataset_CNAqc$snvs
-cna = CNAqc::example_dataset_CNAqc$cna
-purity = CNAqc::example_dataset_CNAqc$purity
-reference = CNAqc::example_dataset_CNAqc$reference
-
-w = pipeline_qc_copynumbercalls(mutations, cna, purity)
-w$plot %>% ggsave(filename = 'a.pdf', width = 14, height = 28)
-
-x = w$plot
-
-hd = ggplot_report_header("FAFDF", paragraph = Sys.time())
-ww = ggarrange(hd, x, heights = c(0.03, 0.97), ncol = 1, nrow = 2)
-ww %>% ggsave(filename = 'a.pdf', width = 14, height = 28)
+# mutations = CNAqc::example_dataset_CNAqc$snvs
+# cna = CNAqc::example_dataset_CNAqc$cna
+# purity = CNAqc::example_dataset_CNAqc$purity
+# reference = CNAqc::example_dataset_CNAqc$reference
+#
+# w = evoverse:::pipeline_qc_copynumbercalls(mutations, cna, purity, smooth = F, collate = F)
 
 #' Title
 #'
@@ -20,6 +13,10 @@ ww %>% ggsave(filename = 'a.pdf', width = 14, height = 28)
 #' @param reference
 #' @param sample
 #' @param matching_epsilon_peaks
+#' @param output
+#' @param smooth
+#' @param cex
+#' @param collate
 #'
 #' @return
 #' @export
@@ -30,112 +27,115 @@ pipeline_qc_copynumbercalls = function(
   cna,
   purity,
   reference = 'GRCh38',
-  sample = "My Sample",
-  matching_epsilon_peaks = 0.025)
+  sample = "PAT00001",
+  output = paste0(sample, '_evoverse_qcreport.pdf'),
+  matching_epsilon_peaks = 0.025,
+  smooth = FALSE,
+  cex = .7,
+  collate = FALSE)
 {
   pio::pioHdr("Evoverse", italic(paste0('~ Pipeline to determine thq quality of somatic mutation and CNA data')))
   cat('\n')
 
+  # Creating input CNAqc
+  cli::cli_h1("Creating input object (smoothing {.field {smooth}}) for sample {.field {sample}}")
+  cat("\n")
+
   x = CNAqc::init(mutations, cna, purity, ref = reference)
+
+  print(x)
+
+  if(smooth) x = CNAqc::smooth_segments(x)
 
   USE_KARYOTYPES = c("1:0", '1:1', '2:0', '2:1', '2:2')
   prevalent_karyo = CNAqc:::get_prevalent_karyotype(x, rank = 1)
 
-  # Segments
-  segments = CNAqc::plot_segments(x)
-  # segments = ggpubr::ggarrange(
-  #   plot_label,
-  #   segments,
-  #   ncol = 1,
-  #   nrow = 2,
-  #   heights = c(1, 4)
-  # )
-
-  ##### ##### ##### ##### ##### ##### ##### #####
   # peak detection ~ QC purity/ ploidy
-  ##### ##### ##### ##### ##### ##### ##### #####
+  cli::cli_h1("Purity/ ploidy QC via peak detection")
+  cat("\n")
+
   x = CNAqc::analyze_peaks(x,
                            matching_epsilon = matching_epsilon_peaks,
                            karyotypes = USE_KARYOTYPES)
 
-  adjustment = round(x$peaks_analysis$score * 2, 2)
-
-  peaks_score_title = bquote(
-    bold("  Purity/ploidy QC : ") ~ rho ~'= '* .(x$peaks_analysis$score) ~ ' (' * epsilon ~ '=' ~ .(matching_epsilon_peaks) * ')'
-  )
-
-  peaks = suppressWarnings(suppressMessages(CNAqc::plot_peaks_analysis(x)))
-
-  ##### ##### ##### ##### ##### ##### ##### #####
   # CCF values
-  ##### ##### ##### ##### ##### ##### ##### #####
+  cli::cli_h1("CCF estimation and QC")
+  cat("\n")
+
   x = CNAqc::compute_CCF(x, karyotypes = USE_KARYOTYPES)
 
-  ccf_plot = lapply(
-    USE_KARYOTYPES,
-    function(k)
-    {
-      if(!(k %in% names(x$CCF_estimates))) return(CNAqc:::eplot())
-      suppressWarnings(CNAqc:::plot_mutation_multiplicity_entropy(x, k))
-    }
-  )
-
-  ccf_plot = ggpubr::ggarrange(plotlist = ccf_plot, nrow = length(USE_KARYOTYPES), ncol = 1)
-
-  ccf_plot = ggpubr::annotate_figure(ccf_plot,
-                                  top = ggpubr::text_grob(x = 0, hjust = 0,  label = "  Cancer Cell Fractions (estimated)")
-  )
-
-  ##### ##### ##### ##### ##### ##### ##### #####
   # Fragmentation (after smoothing)
-  ##### ##### ##### ##### ##### ##### ##### #####
-  x = CNAqc::smooth_segments(x)
-  x = CNAqc::detect_arm_overfragmentation(x)
-  frags_plot = suppressWarnings(CNAqc::plot_arm_fragmentation(x, zoom = 0))
+  cli::cli_h1("Detecting patterns of overfragmentation")
+  cat("\n")
 
+  x = CNAqc::detect_arm_overfragmentation(x)
   x =  CNAqc::detect_wg_overfragmentation(x)
 
-
-  ##### ##### ##### ##### ##### ##### ##### #####
-  # Figure assembly
-  ##### ##### ##### ##### ##### ##### ##### #####
-  rpass = function(f) {
-    ggpubr::annotate_figure(f, top = '')
-  }
-
-  figure =
-    suppressWarnings(
-      ggpubr::ggarrange(
-        segments %>% rpass,
-        peaks  %>% rpass,
-        ccf_plot  %>% rpass,
-        frags_plot  %>% rpass,
-        heights = c(1, 1, length(USE_KARYOTYPES), 1),
-        labels = LETTERS[1:4],
-        ncol = 1,
-        nrow = 4
-      )
-    )
-
-  # Report label
-  figure = ggpubr::annotate_figure(
-    figure,
-    top = ggpubr::text_grob(x = 0, hjust = 0,
-                            label = bquote("  Input purity" ~.(purity) ~ "; CNAqc ploidy " ~ .(x$ploidy) * ' (' * .(prevalent_karyo) * ' karyotype)'),
-                            size = 12)
-  )
-
-  figure = ggpubr::annotate_figure(
-    figure,
-    top = ggpubr::text_grob(x = 0, hjust = 0,
-                            label = bquote(bold("  evoverse CNA/ mutation report : ") ~.(sample)), size = 15)
-  )
+  # Final QC
+  QC_table = evoverse:::get_pqc_cna_QC_table(x)
 
   # Return
   fit = list()
   fit$fit = x
-  fit$plot = figure
+
+  fit$QC$QC_table = QC_table
+  fit$QC$f_PASS = sum(QC_table$QC == "PASS")/(sum(QC_table$QC == "PASS") + sum(QC_table$QC == "FAIL")) * 100
+
+  # Figure assembly
+  cat("\n")
+  cli::cli_h1("PDF report assembly")
+  cli::cli_process_start("Creating report in file {.field {output}}")
+  cat('\n')
+
+  suppressWarnings(
+    evoverse:::report_multipage_cnaqc_pipeline(
+      x,
+      f = output,
+      cex = cex,
+      sample = sample,
+      collate = collate,
+      score = fit$QC$f_PASS
+    )
+  )
+  cli::cli_process_done()
+
+  fit$plot_file = output
+
 
   return(fit)
+}
+
+get_pqc_cna_QC_table = function(x)
+{
+  all_karyptypes = x$peaks_analysis$plots %>% names
+
+  peaks_QC = x$peaks_analysis$matches %>%
+    dplyr::select(karyotype, QC) %>%
+    dplyr::full_join(data.frame(karyotype = all_karyptypes, stringsAsFactors = F), by = 'karyotype') %>%
+    dplyr::distinct(karyotype, QC, .keep_all = T) %>%
+    dplyr::arrange(karyotype) %>%
+    dplyr::mutate(
+      value = 1,
+      lab.ypos = cumsum(value) - 0.5 * value,
+      QC = paste(QC),
+      label = karyotype,
+      type = 'Peaks')
+
+  CCF_QC = Reduce(dplyr::bind_rows, lapply(x$CCF_estimates, function(x) x$QC_table)) %>%
+    dplyr::select(karyotype, QC) %>%
+    dplyr::full_join(data.frame(karyotype = all_karyptypes, stringsAsFactors = F), by = 'karyotype') %>%
+    dplyr::arrange(karyotype) %>%
+    dplyr::mutate(
+      value = 1,
+      lab.ypos = cumsum(value) - 0.5 * value,
+      QC = paste(QC),
+      label = karyotype,
+      type = 'CCF')
+
+  QC_table = dplyr::bind_rows(peaks_QC, CCF_QC)
+  QC_table$karyotype = factor(QC_table$karyotype, all_karyptypes)
+  QC_table$type = factor(QC_table$type, levels = c('Peaks', 'CCF'))
+
+  QC_table
 }
 
--- R/util_reports_layout.R
+++ R/util_reports_layout.R
@@ -1,127 +1,367 @@
-ggplot_report_header = function(sample, paragraph = Sys.time())
+button = function(color, radio, size, font = 'plain')
 {
-  blank = ggplot() + theme_void()
+  ggplot(data = data.frame(
+    x = 0,
+    y = 0,
+    label = radio,
+    stringsAsFactors = FALSE
+  ),
+  aes(x = x , y = y, label = label)) +
+    theme_void() +
+    theme(plot.background = element_rect(fill = color)) +
+    geom_text(fontface = font, hjust = 0)
+}
 
-  button = function(color, radio, size, font = 'plain')
-  {
-    ggplot(data = data.frame(
-      x = 0,
-      y = 0,
-      label = radio,
-      stringsAsFactors = FALSE
+# devtools::install_github("tarkomatas/myprettyreport")
+
+# Raw data panel
+get_raw_data_panel = function(x)
+{
+  USE_KARYOTYPES = c("1:0", '1:1', '2:0', '2:1', '2:2')
+
+  # Raw mutation data
+  raw_muts = x$snvs %>% dplyr::mutate(karyotype = ifelse(karyotype %in% USE_KARYOTYPES, karyotype, "other"))
+  ccf_data = Reduce(dplyr::bind_rows,
+                    lapply(x$CCF_estimates, function(x)
+                      x$mutations)) %>%
+    dplyr::mutate(karyotype = ifelse(karyotype %in% USE_KARYOTYPES, karyotype, "other"))
+
+  karyo_colors = CNAqc:::get_karyotypes_colors(c(USE_KARYOTYPES, 'other'))
+
+  cvgp = ggplot(data = raw_muts, aes(DP, fill = karyotype)) +
+    geom_histogram(bins = 100) +
+    scale_x_log10() +
+    CNAqc:::my_ggplot_theme() +
+    labs(title = "Sequencing depth (coverage)",
+         caption = paste0("Median coverage ", median(x$snvs$DP), 'x')) +
+    geom_vline(
+      xintercept = median(raw_muts$DP),
+      color = 'orange',
+      linetype = 'dashed',
+      size = .3
+    ) +
+    scale_fill_manual(values = karyo_colors)
+
+  vafp = ggplot(data = raw_muts, aes(VAF, fill = karyotype)) +
+    geom_histogram(binwidth = 0.01) +
+    xlim(0, 1) +
+    CNAqc:::my_ggplot_theme() +
+    labs(title = "Raw VAF",
+         caption = paste0("VAF < 0.05 (5%) = ", sum(x$snvs$VAF < 0.05))) +
+    scale_fill_manual(values = karyo_colors)
+
+  ccfp = ggplot(data = ccf_data,
+                aes(CCF, fill = karyotype)) +
+    geom_histogram(binwidth = 0.01) +
+    xlim(0, max(ccf_data$CCF, na.rm = T) %>% ceiling) +
+    CNAqc:::my_ggplot_theme() +
+    labs(title = "CCF values",
+         caption = paste0("CCF < 0.05 (5%) = ", sum(ccf_data$CCF < 0.05, na.rm = T))) +
+    scale_fill_manual(values = karyo_colors)
+
+  return(list(cvgp, vafp, ccfp))
+}
+
+# Annotate title on top of a plot
+annot_t = function(x, t) {
+  ggpubr::annotate_figure(x,
+                          top = ggpubr::text_grob(
+                            t,
+                            hjust = 0,
+                            x = 0,
+                            face = 'plain'
+                          ))
+
+}
+
+# Add margins to a plot
+fun_marg = function(x, fact, w = 1, h = 1) {
+  x + theme(plot.margin = unit(c(1 * h, 1 * w, 1 * h, 1 * w) * fact, "cm"))
+}
+
+
+page_one = function(x) {
+  N = ifelse(x$n_snvs > 3000, 3000, x$n_snvs)
+
+  # Segments
+  A = cowplot::plot_grid(
+    CNAqc::plot_depth(x, N = N) + labs(
+      title = paste0(
+        "Ploidy ",
+        x$ploidy,
+        ', most prevalent karyotype is ',
+        CNAqc:::get_prevalent_karyotype(x),
+        " (Major:minor).\n"
+      )
     ),
-    aes(x = x , y = y, label = label)) +
-      theme_void() +
-      theme(plot.background = element_rect(fill = color)) +
-      geom_text(fontface = font, hjust = 0)
-  }
-
-  tline = ggpubr::ggarrange(
-    button('gray', sample, size = 10, font = 'bold'),
-    blank,
-    ncol = 2,
-    nrow = 1,
-    widths = c(.7, .3)
+    CNAqc::plot_segments(x),
+    align = 'v',
+    nrow = 2,
+    rel_heights = c(.35, .8)
   )
 
-  dtpar = button('gainsboro', paragraph, size = 10)
+  Bpl  = evoverse:::get_raw_data_panel(x)
 
-  figure = ggpubr::ggarrange(
-    blank,
-    tline,
-    dtpar,
-    blank,
+  B = Bpl[[1]]
+  C = Bpl[[2]]
+  D = Bpl[[3]]
+
+  W = ggpubr::ggarrange(C,
+                        B,
+                        D,
+                        ncol = 3,
+                        common.legend = T,
+                        legend = 'bottom')
+
+  # Genome distribution and QC
+  E = ggpubr::ggarrange(
+    CNAqc::plot_karyotypes(x),
+    CNAqc::plot_karyotypes(x, type = 'number'),
+    common.legend = TRUE,
+    legend = 'bottom'
+  )
+
+  G = CNAqc::plot_qc(x) + labs(title = paste0("Purity/ ploidy (peaks) and CCF estimates (% assignable)"))
+
+
+  H = cowplot::plot_grid(
+    E,
+    G,
+    ncol = 2,
+    rel_widths = c(1, 1),
+    labels = c("C", 'D'),
+    align = 'h',
+    axis = 'x'
+  )
+
+  ggpubr::ggarrange(
+    A,
+    ggplot() + geom_blank(),
+    W,
+    ggplot() + geom_blank(),
+    H,
     ncol = 1,
-    nrow = 4,
-    heights = c(.15, 1, .7, .2)
+    heights = c(5.8, 1, 4, 1, 4.5),
+    labels = c("A", '', "B", '', "C")
   )
 }
 
 
-devtools::install_github("tarkomatas/myprettyreport")
+page_two = function(x)
+{
+  sc = getOption('CNAqc_cex', default = 1)
 
-library(ggplot2)
-sample_plot <- ggplot(data = mtcars, mapping = aes(x = wt, y = mpg)) +
-  geom_point() +
-  stat_smooth(method = 'lm', color = "#f44242", fill = "#fd9068")
-
-library(magick)
-sample_logo <- image_read("https://caravagn.github.io/CNAqc/reference/figures/logo.png")
-
-
-header_front = paste0(
-  'Data quality check (QC) from matched Copy Number and mutation data.'
-)
-
-
-
-segments_plot = suppressMessages(suppressWarnings(
-  cowplot::plot_grid(
-    plot_counts(x),
-    # plot_vaf(x, N = 10000),
-    plot_depth(x, N = 5000),
-    plot_segments(x),
+  A = cowplot::plot_grid(
+    ggplot() + theme_void(base_size = 10  * sc) + labs(
+      title = paste0(
+        "Purity and ploidy QC via peak detection (purity p = ",
+        x$purity * 100,
+        "%)\n"
+      )
+    ),
+    CNAqc::plot_peaks_analysis(x),
     align = 'v',
-    nrow = 4,
-    rel_heights = c(.15, .15, .8)
+    nrow = 2,
+    rel_heights = c(.1, .95)
   )
-))
 
-first_page = suppressMessages(suppressWarnings(
+  N = x$n_snvs * 0.01
+  D = median(x$cna$length) * .1
+
+  segment_ids = x$cna %>%
+    filter(n > !!N, length > D) %>%
+    pull(segment_id)
+
+  if(length(segment_ids) == 0) B = CNAqc:::eplot()
+  else
+  {
+    B = CNAqc::inspect_segment(x, n = N, l = D) +
+      labs(
+        title = paste0("Segments with >", N, " mutations (1% of total) and >", D, ' bases (10% of median length)\n'),
+        subtitle = NULL
+      )
+  }
+
+  C1 = CNAqc::plot_arm_fragmentation(x) +
+    labs(title = paste0("Arm-level segments fragmentation (% relative to arm length)\n"))
+  C2 = CNAqc::plot_segment_size_distribution(x) +
+    labs(title = paste0("Segments size distribution\n"))
+
   cowplot::plot_grid(
-    # button(color = 'white', radio = ""),
-    segments_plot + labs(title = "CNA segments") + theme(plot.margin=unit(c(3,1,1,1) * .3,"cm")),
-    button(color = 'white', radio = paste0("Peak detection for purity ", x$purity * 100, '%')),
-    peaks + theme(plot.margin=unit(c(2,1,2,1) * .3,"cm")),
-    # button(color = 'white', radio = ""),
-    cowplot::plot_grid(
-      plot_arm_fragmentation(x),
-      plot_segment_size_distribution(x),
-      ncol = 2,
-      rel_widths = c(2,1),
-      align = 'v',
-      axis = 'y'
-      ) + theme(plot.margin=unit(c(2,1,3,1) * .3,"cm")),
-    nrow = 4,
+    A,
+    ggplot() + geom_blank(),
+    B,
+    ggplot() + geom_blank(),
+    ggpubr::ggarrange(C1, C2),
     ncol = 1,
-    rel_heights = c(1.5, .05, 1, 1.5)
-    # scale = c(1, .9, 1, .8, 1, .9)
-  )))
+    rel_heights = c(4, 1, 4, 1, 4),
+    labels = c("A", '', "B", '', "C"),
+    scale = c(.9, 1, 1, 1, 1),
+    align = 'v',
+    axis = 'y'
+  )
 
-second_page = suppressMessages(suppressWarnings(
-  cowplot::plot_grid(
-    ccf_plot,
-    nrow = 1,
-    ncol = 1
-    # scale = c(1, .9, 1, .8, 1, .9)
-  )))
+}
 
+page_three = function(x)
+{
+  CNAqc::plot_CCF(x) %>%
+    annot_t(t = paste0(
+      "QC PASS (per karyotype) if we can assign >90% of mutation burden\n"
+    ))
+}
+
+# Report multi-page style for the Data QC pipeline
+report_multipage_cnaqc_pipeline = function(x, f, cex = .7, sample, score, collate = F)
+{
+  # Set global cex value for all CNAqc plots
+  options(CNAqc_cex = cex)
+  require(CNAqc)
+  require(patchwork)
+
+  # QC
+  PASS = score
+  l_PASS = cut(PASS, seq(0, 100, by = 20)) %>% as.numeric
+  qc_col = brewer.pal('Spectral', n = 5)[l_PASS]
+
+  # Page one -- segments | vaf | qc
+  first_page = evoverse:::page_one(x) %>% evoverse:::fun_marg(fact = 1, w = 1, h = )
+
+  # Page two -- Peak analysis and fragmentation
+  second_page = evoverse:::page_two(x) %>% evoverse:::fun_marg(fact = 1, w = 1, h = 2)
+
+  # Page two -- Peak analysis and fragmentation
+  third_page = evoverse:::page_three(x) %>% evoverse:::fun_marg(fact = 1, w = 1, h = 2)
+
+  # Report assembly
+  require(myprettyreport)
+  require(magick)
+
+  tool <-
+    image_read("https://caravagn.github.io/CNAqc/reference/figures/logo.png")
+  sample_logo = tool
+  # frink <- image_read("https://jeroen.github.io/images/frink.png")
+  # sample_logo = c(tool, frink)
+
+  cp_e_params = function() {
+    B = brewer.pal('Spectral', n = 5)
+
+
+    # for(i in seq_along(B))
+    # {
+    #   grid::grid.rect(gp = grid::gpar(fill = B[i]),
+    #                   vp = grid::viewport(layout.pos.row = 2,
+    #                                       layout.pos.col = 2),
+    #                   x = i,
+    #                   y = 2,
+    #                   width = 0.2, height = 0.2)
+    # }
 #
-# library(myprettyreport)
-start_report() %>%
-  add_cover_page(
-    logo = sample_logo,
-    title = sample,
-    subtitle = header_front,
-    logo_size = 0.15,
-    need_footer = T
-  ) %>%
-  add_new_page(
-    plot = first_page,
-    title = sample,
-    subtitle = 'Segments data and purity/ ploidy QC.',
-    need_header = TRUE,
-    logo = sample_logo,
-    logo_size = 0.1
-  ) %>%
-  add_new_page(
-    plot = second_page,
-    title = sample,
-    subtitle = 'Cancer Cell Fractions data',
-    need_header = TRUE,
-    logo = sample_logo,
-    logo_size = 0.1
-  ) %>%
-  end_report()
+# #
+    grid::grid.rect(
+      gp = grid::gpar(fill = alpha(qc_col, .3)),
+      vp = grid::viewport(layout.pos.row = 2,
+                          layout.pos.col = 1),
+      width = 0.8,
+      height = 0.2
+    )
+#
+    grid::grid.text(paste0("PASS rate ", score), gp=grid::gpar(col=qc_col),
+                    vp = grid::viewport(layout.pos.row = 2,
+                                        layout.pos.col = 1))
+  }
+
+  p1_tit = 'Data quality check (QC) from matched Copy Number and mutation data.'
+  p2_tit = 'Purity/ ploidy QC (A), data (B) and fragmentation (C).'
+  p3_tit = 'Cancer Cell Fractions estimation and QC.'
+
+  # Special report - 1 page wide collated
+  if(collate == FALSE)
+  {
+    message("Saving single-page collated PDF report")
+
+    ggpubr::ggarrange(
+      first_page %>% evoverse:::annot_t(p1_tit),
+      second_page %>% evoverse:::annot_t(p2_tit),
+      third_page %>% evoverse:::annot_t(p3_tit),
+      ncol = 3
+    ) %>%
+      ggsave(filename = f, height = 250, width = 2.1 * 297, units = 'mm')
+  }
+  else
+  {
+    message("Saving multi-page PDF report")
+
+    start_report(size = 'a4', filename = f) %>%
+      add_cover_page(
+        logo = sample_logo,
+        title = sample,
+        subtitle = p1_tit,
+        logo_size = 0.15,
+        theme = 'basic',
+        creaton_time = paste0(
+          'evoverse pipeline (caravagn.github.io/evoverse), ',
+          Sys.time()
+        ),
+        need_footer = T,
+        extra_layout_params = cp_e_params
+      ) %>%
+      add_new_page(
+        plot = first_page,
+        title = sample,
+        subtitle = 'CNA Segments (A), mutations (B), genome coverage (C) and QC (D).',
+        need_header = TRUE,
+        header_color = "#f44242",
+        logo = sample_logo,
+        logo_size = .35,
+        theme = "flashy",
+        need_footer = TRUE,
+        footer_text = "Copyright © Giulio Caravagna | 2020 | evoverse (caravagn.github.io/evoverse)",
+        footer_color = "#3d3c3c"
+      ) %>%
+      add_new_page(
+        plot = second_page,
+        title = sample,
+        subtitle = p2_tit,
+        need_header = TRUE,
+        header_color = "#f44242",
+        logo = sample_logo,
+        logo_size = .35,
+        theme = "flashy",
+        need_footer = TRUE,
+        footer_text = "Copyright © Giulio Caravagna | 2020 | evoverse (caravagn.github.io/evoverse)",
+        footer_color = "#3d3c3c"
+      ) %>%
+      add_new_page(
+        plot = third_page,
+        title = sample,
+        subtitle = p3_tit,
+        need_header = TRUE,
+        header_color = "#f44242",
+        logo = sample_logo,
+        logo_size = .35,
+        theme = "flashy",
+        need_footer = TRUE,
+        footer_text = "Copyright © Giulio Caravagna | 2020 | evoverse (caravagn.github.io/evoverse)",
+        footer_color = "#3d3c3c"
+      ) %>%
+      end_report()
+  }
+
+}
 
 
+# report_multipage_cnaqc_pipeline(x)
+#
+# library(pdftools)
+#
+# pdf_combine(c("report_output.pdf", "report_output.pdf"), output = "joined.pdf")
+# pdf_compress('joined.pdf', output = "joined3.pdf")
+#
+#
+#
+# plotflow:::mergePDF(
+#   in.file = paste(c("report_output.pdf", "report_output.pdf"), collapse=" "),
+#   file="joined3.pdf"
+# )
+#
