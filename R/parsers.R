5e6999b8a3738a795aacd3b3a881ab2acc98b104
caravagn <gcaravagn@gmail.com>
2020-03-02 14:35:07 +0000

    Parser for multi-sample VCFs

--- R/parsers.R
+++ R/parsers.R
@@ -93,18 +93,23 @@ evoparse_platypus_mutations = function(x)
     stop(paste0("Missing required VCF file: ", x))
 
   # Parsing with vcfR -- maybe Variant Annontation works better..
-  xfile = vcfR::read.vcfR(x)
+  xfile = vcfR::read.vcfR(x, verbose = F)
 
   # INFO field does not come out very nice -- so I omit it
-  info_field = vcfR::extract_info_tidy(xfile)
+  # info_field = vcfR::extract_info_tidy(xfile)
 
   # Genotype(s)
-  gt_field = vcfR::extract_gt_tidy(xfile) %>%
+  gt_field = vcfR::extract_gt_tidy(xfile, verbose = F)
+
+  if(!all(c("gt_NR", "gt_NV", "Indiv") %in% colnames(gt_field))) stop("Missing required fields in the VCF (gt_NR, gt_NV, Indiv)")
+
+  gt_field = gt_field %>%
     dplyr::mutate(
       DP = as.numeric(gt_NR),
       NV = as.numeric(gt_NV),
       VAF = NV/DP
-    )
+    ) %>%
+    dplyr::rename(sample = Indiv)
 
   na_muts = gt_field %>% dplyr::filter(is.na(VAF))
 
@@ -114,7 +119,11 @@ evoparse_platypus_mutations = function(x)
 
   # Fixed fields -- this should retain everything relevant
   fix_field = xfile@fix %>%
-    as_tibble() %>%
+    as_tibble()
+
+  if(!all(c("CHROM", "POS", "REF", "ALT") %in% colnames(fix_field))) stop("Missing required fields in the VCF (CHROM, POS, REF, ALT)")
+
+  fix_field = fix_field %>%
     dplyr::rename(
       chr = CHROM,
       from = POS,
@@ -129,19 +138,122 @@ evoparse_platypus_mutations = function(x)
     dplyr::ungroup() %>%
     dplyr::select(chr, from, to, ref, alt, dplyr::everything())
 
-  if(nrow(fix_field) != nrow(gt_field))
-    stop("Mismatch between the VCF fixed fields and the genotypes, will not process this file.")
+  # Extract each sample
+  samples_list = gt_field$sample %>% unique
 
-  fits = list()
-  fits$input = x
-  fits$mutations = dplyr::bind_cols(fix_field, gt_field) %>%
-    dplyr::select(chr, from, to, ref, alt, NV, DP, VAF, dplyr::everything())
+  calls = lapply(
+    samples_list,
+    function(s){
+      gt_field_s = gt_field %>% dplyr::filter(sample == s)
+
+      if(nrow(fix_field) != nrow(gt_field_s))
+        stop("Mismatch between the VCF fixed fields and the genotypes, will not process this file.")
+
+      fits = list()
+      fits$input = x
+      fits$sample = s
+      fits$caller = "Platypus"
+      fits$mutations = dplyr::bind_cols(fix_field, gt_field_s) %>%
+        dplyr::select(chr, from, to, ref, alt, NV, DP, VAF, dplyr::everything())
+
+      fits
+    })
+
+  names(calls) = samples_list
 
   cli::cli_process_done()
 
-  return(fits)
+  return(calls)
 }
 
-# Sequenza_calls = evoparse_Sequenza_CNA(x)
+#' Pares Platypus mutation data.
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+evoparse_mutect_mutations = function(x, sample = NULL)
+{
+  cli::cli_process_start("Evoverse parser for Mutect mutation calls from VCF file {.field {x}}")
+  cat('\n')
 
+  if(!file.exists(x))
+    stop(paste0("Missing required VCF file: ", x))
+
+  # Parsing with vcfR -- maybe Variant Annontation works better..
+  xfile = vcfR::read.vcfR(x, verbose = F)
+
+  # INFO field does not come out very nice -- so I omit it
+  # info_field = vcfR::extract_info_tidy(xfile)
+
+  # Genotype(s)
+  gt_field = vcfR::extract_gt_tidy(xfile, verbose = F)
+
+  if(!all(c("gt_AD", "Indiv") %in% colnames(gt_field))) stop("Missing required fields in the VCF (gt_AD, Indiv)")
+
+  gt_field = gt_field %>%
+    tidyr::separate(gt_AD, sep = ',', into = c("NR", "NV")) %>%
+    dplyr::mutate(
+      NR = as.numeric(NR),
+      NV = as.numeric(NV),
+      DP = NV + NR,
+      VAF = NV/DP
+    ) %>%
+    dplyr::rename(sample = Indiv)
+
+  na_muts = gt_field %>% dplyr::filter(is.na(VAF))
+
+  if(nrow(na_muts) > 0) {
+    cat("There are n =", nrow(na_muts), "mutations with VAF that is NA; they will NOT be removed.\n")
+  }
+
+  # Fixed fields -- this should retain everything relevant
+  fix_field = xfile@fix %>%
+    as_tibble()
+
+  if(!all(c("CHROM", "POS", "REF", "ALT") %in% colnames(fix_field))) stop("Missing required fields in the VCF (CHROM, POS, REF, ALT)")
+
+  fix_field = fix_field %>%
+    dplyr::rename(
+      chr = CHROM,
+      from = POS,
+      ref = REF,
+      alt = ALT
+    ) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(
+      from = as.numeric(from),
+      to = from + nchar(alt)
+    ) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(chr, from, to, ref, alt, dplyr::everything())
+
+  # Extract each sample
+  samples_list = gt_field$sample %>% unique
+
+  calls = lapply(
+    samples_list,
+    function(s){
+      gt_field_s = gt_field %>% dplyr::filter(sample == s)
+
+      if(nrow(fix_field) != nrow(gt_field_s))
+        stop("Mismatch between the VCF fixed fields and the genotypes, will not process this file.")
+
+      fits = list()
+      fits$input = x
+      fits$sample = s
+      fits$caller = "Mutect"
+      fits$mutations = dplyr::bind_cols(fix_field, gt_field_s) %>%
+        dplyr::select(chr, from, to, ref, alt, NV, DP, VAF, dplyr::everything())
+
+      fits
+    })
+  names(calls) = samples_list
+
+  cli::cli_process_done()
+
+  return(calls)
+}
 
