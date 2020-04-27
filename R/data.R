8cabc40a6e0c32f9de7c6563be26be8d43d47428
Giulio Caravagna <gcaravagn@gmail.com>
2020-04-27 13:54:33 +0200

    Refactoring

--- R/data.R
+++ R/data.R
@@ -1,38 +1,3 @@
-#' Example \code{evoverse} dataset.
-#'
-#' @docType data
-#'
-#' @usage data(example_evoverse)
-#'
-#' @format A simple dataset created and analyzed with the
-#' \code{evoverse} package.
-#'
-#' @keywords datasets
-#'
-#' @examples
-#' data(example_evoverse)
-#' head(example_evoverse)
-#'
-"example_evoverse"
-
-#' Example data in the wide and long formts supported by the
-#' \code{evoverse} package.
-#'
-#' @docType data
-#'
-#' @usage data(example_input_formats)
-#'
-#' @format 4 tibbles, wide/long and mutations/copy number that
-#' show the format required to create an  \code{evoverse} object with
-#' function \link{\code{dataset}}.
-#'
-#' @keywords datasets
-#'
-#' @examples
-#' data(example_input_formats)
-#' head(example_input_formats)
-"example_input_formats"
-
 #' QC pre-trained Logistic classifier for timing of CNA with MOBSTER.
 #'
 #' @docType data
@@ -46,3 +11,33 @@
 #' @examples
 #' print(qc_timing_model)
 "qc_timing_model"
+
+#' QC pre-trained Logistic classifier for deconvolution with a single Beta component with MOBSTER
+#' (monoclonal case).
+#'
+#' @docType data
+#'
+#' @usage data(qc_deconvolution_monoclonal)
+#'
+#' @format A logistic classifier.... describe training ......
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' print(qc_deconvolution_monoclonal)
+"qc_deconvolution_monoclonal"
+
+#' QC pre-trained Logistic classifier for deconvolution with multiple Beta component with MOBSTER
+#' (polyclonal case).
+#'
+#' @docType data
+#'
+#' @usage data(qc_deconvolution_polyclonal)
+#'
+#' @format A logistic classifier.... describe training ......
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' print(qc_deconvolution_polyclonal)
+"qc_deconvolution_polyclonal"
