8150d17ddaebd7273d75c8bf7a093987d41e853b
salvatore <militesalvatore@gmail.com>
2021-04-19 15:32:57 +0200

    mobsterh pipeline

--- R/PIPELINE_DECONV_VAF_HIERARCHICAL.R
+++ R/PIPELINE_DECONV_VAF_HIERARCHICAL.R
@@ -173,7 +173,7 @@ plot.evopipe_rawkh = function(x, ...)
 
   figure = ggpubr::ggarrange(
     CNAqc::plot_segments(cna_obj, circular = FALSE, highlight = groups) + labs(title = x$description),
-    CNAqc::plot_peaks_analysis(cna_obj),
+    CNAqc::plot_peaks_analysis(cna_obj, empty_plot = F),
     mob_fits_plot,
     nrow = 3,
     ncol = 1
